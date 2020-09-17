73e5f77bf41451bcfda8186e4f4ee725e63873af
Samar-100 <67325192+Samar-100@users.noreply.github.com>
2020-09-17 18:47:44 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,48 @@
+//Create variables here
+var dog, happydDog, database, foodStock, foodS;
+
+function preload() {
+  //load images here
+  dogimg = loadImage("images/dogImg.png");
+  dogimg2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  var canvas = createCanvas(500, 500);
+  database = firebase.database();
+  foodStock = database.ref("Food");
+  foodStock.on("value", readStock);
+  dog = createSprite(250, 250, 10, 10);
+  dog.addImage(dogimg);
+  dog.scale = 0.2;
+}
+
+function draw() {
+  background(46, 139, 87);
+  if (keyWentDown(UP_ARROW)) {
+    writeStock(foodS);
+    dog.addImage(dogimg2);
+  }
+
+  drawSprites();
+  //add styles here
+  fill("blue");
+  textSize(20);
+  stroke(5);
+  text("Press Up Arrow Kew To Feed drago milk", 80, 100);
+  text("You have: " + foodS + " Milk left", 100, 70);
+  console.log(foodS);
+}
+function readStock(data) {
+  foodS = data.val();
+}
+function writeStock(x) {
+  if (x <= 0) {
+    x = 0;
+  } else {
+    x = x - 1;
+  }
+  database.ref("/").update({
+    food: x,
+  });
+}
